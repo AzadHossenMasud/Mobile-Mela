6d4ecc36b1bb64e899f934f2f19ab8dcd54680b5
Azad Hossen <azadhossen6677@gmail.com>
2022-11-27 00:07:57 +0600

    register

--- src/context/AuthProvider/AuthProvider.js
+++ src/context/AuthProvider/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from "firebase/auth";
 import app from '../../firebase/firebase.config';
 
 
@@ -11,7 +11,17 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
 
-    const authInfo ={}
+
+    const createUser = (email, password)=>{
+        return createUserWithEmailAndPassword(auth, email, password)
+
+    }
+
+    const updateUser = (userInfo)=>{
+       return updateProfile(auth.currentUser, userInfo)
+    }
+
+    const authInfo ={createUser, updateUser}
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,11 +1,128 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { AuthContext } from "../../context/AuthProvider/AuthProvider";
 
 const Register = () => {
-    return (
-        <div>
-            this is register
-        </div>
-    );
+  const { createUser, updateUser } = useContext(AuthContext);
+  //   console.log(createUser);
+  const { register, handleSubmit } = useForm();
+  const handleRegister = (data) => {
+    createUser(data.email, data.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user)
+
+        const userInfo = {
+            displayName: data.name
+        }
+
+        updateUser(userInfo)
+        .then(() => {
+            toast.success(`${data.name}, You are successfully registered.`);
+
+            saveUser(data.name, data.email, data.userType);
+
+            // Profile updated!
+            // ...
+          }).catch((error) => {
+            // An error occurred
+            // ...
+          });
+
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        toast.error(errorMessage);
+        // ..
+      });
+  };
+
+  const saveUser = (name, email, userType) => {
+    const userInfo = {
+      name,
+      email,
+      userType,
+    };
+
+    fetch("http://localhost:5000/users", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    // console.log(userInfo);
+  };
+  return (
+    <div className=" w-96 mx-auto my-10 font-semibold text-purple-900 shadow-xl flex justify-center items-center rounded-lg">
+      <div className=" ">
+        <h1 className=" text-2xl text-center">Sign Up Here!</h1>
+        <form onSubmit={handleSubmit(handleRegister)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text text-purple-900">Name:</span>
+            </label>
+            <input
+              type="text"
+              className="input input-bordered w-full max-w-xs"
+              {...register("name", { required: true, maxLength: 20 })}
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text text-purple-900">Email:</span>
+            </label>
+            <input
+              type="email"
+              className="input input-bordered w-full max-w-xs"
+              {...register("email", { required: true })}
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text text-purple-900">Password:</span>
+            </label>
+            <input
+              type="password"
+              className="input input-bordered w-full max-w-xs"
+              {...register("password", { required: true, minLength: 6 })}
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text text-purple-900">User Type:</span>
+            </label>
+            <select
+              className="select select-bordered w-full max-w-xs"
+              {...register("userType", { required: true })}
+            >
+              <option selected value="buyer">
+                Buyer
+              </option>
+              <option value="seller">Seller</option>
+            </select>
+          </div>
+          <div className=" flex justify-center">
+            <input
+              className="btn btn-sm mx-auto bg-purple-800 my-3"
+              type="submit"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
